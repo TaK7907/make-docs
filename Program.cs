8beec24614a750a9ae50f7353b0adb13ce5525c9
TaK7907 <25642298+TaK7907@users.noreply.github.com>
2021-11-05 06:08:31 +0900

    initial commit.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using Zx;
+using static Zx.Env;
+
+await "plantuml -o ../img -tsvg diagrams";
+await "pandoc --defaults doc/defaults.yaml";
---
+++ doc/01-chapter1.md
@@ -0,0 +1,9 @@
+# 第1章
+
+あいうえお
+
+![Diagram 1](../img/diagram1.svg)
+
+かきくけこ
+
+![Diagram 2](../img/diagram2.svg)
